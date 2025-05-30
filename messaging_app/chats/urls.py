e652b37b48c1f34ce8736d8c6a13286d0fd3cba3
se-thato <thatoselepe80@gmail.com>
2025-05-30 23:38:23 +0200

    routers changes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 
@@ -8,8 +9,13 @@ router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+# Nested router for messages under conversations
+conversations_router = router.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/', include(conversations_router.urls)), 
 ]
 
 
