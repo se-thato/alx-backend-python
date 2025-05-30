4c3b04132960c9e277ba3bc5a74a581997aeb26d
se-thato <thatoselepe80@gmail.com>
2025-05-30 19:36:23 +0200

    models commit

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True, null= True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
+
+
+#creating conversation model
+class Conversion(models.Model):
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Conversation {self.id} with {self.participants.count()} participants"
+    
+
+#creating message model
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversion, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.id} from {self.sender.username} in Conversation {self.conversation.id}"
+    
