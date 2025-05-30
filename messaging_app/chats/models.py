ef54e273df95c5f68de8782c6f7ed166b00bea1f
se-thato <thatoselepe80@gmail.com>
2025-05-30 19:51:10 +0200

    made imports changes

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from django.conf import settings
+import uuid
 
 
 class User(AbstractUser):
@@ -17,7 +19,8 @@ class User(AbstractUser):
 
 
 #creating conversation model
-class Conversion(models.Model):
+class Conversation(models.Model):
+    conversation_id = models.CharField(max_length=255, unique=True)
     participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -28,10 +31,11 @@ class Conversion(models.Model):
 
 #creating message model
 class Message(models.Model):
-    conversation = models.ForeignKey(Conversion, on_delete=models.CASCADE, related_name='messages')
+    message_id = models.CharField(max_length=255, unique=True)
+    message_body = models.TextField()
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Message {self.id} from {self.sender.username} in Conversation {self.conversation.id}"
