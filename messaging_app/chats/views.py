d88f03b4b21c5cbc74e0096430d180226c8edddf
se-thato <thatoselepe80@gmail.com>
2025-05-30 21:43:21 +0200

    views setup

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .serializers import ConversationSerializer, MessageSerializer
+from .models import Conversation, Message
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
