da415c64950b1391473a4ce0e8211b734e58f649
se-thato <thatoselepe80@gmail.com>
2025-05-30 23:11:11 +0200

    made some changes

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,22 +3,29 @@ from .serializers import ConversationSerializer, MessageSerializer
 from .models import Conversation, Message
 from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 
 
-
+#this will be used to create views for the Conversation and Message models
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
+    """
+    getting all conversations
+    then serializing them
+    and providing permissions for authenticated users
+    """
+    queryset = Conversation.objects.all() 
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
-
+# this will be used to create views for the Message model
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    #allwing filtering on the conversation field
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['conversation']
+    filterset_fields = ['conversation']  # this will allow us to filter messages by conversation
 
 
