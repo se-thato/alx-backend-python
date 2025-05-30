a1198f69b96293d725ef1f4b9cd69179ae72bc55
se-thato <thatoselepe80@gmail.com>
2025-05-30 23:04:40 +0200

    routers commit

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+
+routers = DefaultRouter()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("chats.urls")),
+
+    path('api/', include(routers.urls)),
 ]
+
