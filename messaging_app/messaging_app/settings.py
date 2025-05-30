9d96d4ea4d41fbbce2205c6be34b37454730bfcf
se-thato <thatoselepe80@gmail.com>
2025-05-30 19:15:11 +0200

    auth setup

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -118,6 +118,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         
     ),
     'DEFAULT_PERMISSION_CLASSES': (
