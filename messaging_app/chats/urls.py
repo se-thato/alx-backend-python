4dd2ff2854eba99badcd5fc5ffd4949c7d4db6af
se-thato <thatoselepe80@gmail.com>
2025-06-01 22:09:20 +0200

    add message app commit

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_nested import NestedDefaultRouter
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 
@@ -10,7 +9,7 @@ router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 # Nested router for messages under conversations
-conversations_router = router.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
 conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
