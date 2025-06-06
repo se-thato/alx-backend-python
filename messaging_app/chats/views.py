eb6145792b90c5bb23c1227060930e30088e32e0
se-thato <thatoselepe80@gmail.com>
2025-06-06 21:13:04 +0200

    authentication

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
 from .permissions import IsParticipantOrReadOnly, IsParticipantOfConversation
+from rest_framework.exceptions import PermissionDenied
 
 #this will be used to create views for the Conversation and Message models
 class ConversationViewSet(viewsets.ModelViewSet):
@@ -31,4 +32,33 @@ class MessageViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['conversation']  # this will allow us to filter messages by conversation
 
+    def get_queryset(self):
+        user = self.request.user
+        # Only show messages from conversations the user participates in
+        return Message.objects.filter(conversation__participants=user)
+    
+
+    def perform_create(self, serializer):
+        conversation = serializer.validated_data.get('conversation')
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied("You are not a participant in this conversation.")
+        serializer.save()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
 
