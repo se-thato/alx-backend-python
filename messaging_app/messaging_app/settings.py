4e583c795763fa33c3c2227c4e3422e45bfa2406
se-thato <thatoselepe80@gmail.com>
2025-06-27 00:41:37 +0200

    Docker Compose

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,11 +83,14 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DB'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'MYSQL_ROOT_PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
